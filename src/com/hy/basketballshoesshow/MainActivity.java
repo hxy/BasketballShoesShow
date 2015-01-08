cbc309b49d94e71b373fbc1f6ca58fc16eedacfc
yue.huang <yue.huang@connexis.com>
2015-01-08 10:18:29 +0800

    change packagename

--- src/com/hy/basketballshoesshow/MainActivity.java
+++ src/com/hy/basketballshoesshow/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.basketballshoesshow;
+package com.hy.basketballshoesshow;
 
+import com.hy.basketballshoesshow.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
